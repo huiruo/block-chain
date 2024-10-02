54e95a74f45a875099dc7c69464b2faf45bf0ae3
ruo <ruomechen@gmail.com>
2024-10-02 16:27:30 +0800

    fix: build

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,23 +1,7 @@
 import { LayoutWrapper } from "@/components/layout/layoutWrapper";
 
-<<<<<<< HEAD
-const Dashboard: React.FC = () => {
-
-  return <div className='wrapper-box'>
-    <div className='wrapper-container'>
-      <Layout />
-      <div className='md-box'>
-        <h1>Dashboard</h1>
-      </div>
-    </div>
-  </div>
-};
-
-export default Dashboard
-=======
 export default function Home() {
   return <LayoutWrapper>
     dashboard
   </LayoutWrapper>
 }
->>>>>>> 74ba2008fa5009e2244dd6de9d6edf682ce4c9d2
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,8 @@ import styles from "./page.module.css";
 import { LayoutWrapper } from "@/components/layout/layoutWrapper";
 
 export default function Home() {
-<<<<<<< HEAD
-  return <div className='wrapper-box'>
-    <div className='wrapper-container'>
-      <Layout />
-      <div className='md-box'>
-        Home
-      </div>
-    </div>
-  </div>
-=======
   return <LayoutWrapper>
     <main className={styles.main}>
     </main>
   </LayoutWrapper>
->>>>>>> 74ba2008fa5009e2244dd6de9d6edf682ce4c9d2
 }

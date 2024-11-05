44ea3a5085d42d6f2fb4a2d472a7b7c95eec4af2
ruo <ruomechen@gmail.com>
2024-11-05 15:50:22 +0800

    feat: update link

--- src/app/elections/page.tsx
+++ src/app/elections/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Elections from '../../mdx/crypto/elections.mdx';
 
 const Page: React.FC = () => {
-  return <div style={{marginTop: '60px'}}>
+  return <div style={{ marginTop: '60px', marginBottom: '100px' }}>
     <iframe
       title="election-map"
       src="https://polymarket.com/embed/elections/map?event=presidency&theme=dark&creator=0x841e9627B67Ca2E957e8c55B4cF28Ec43B93651D-1730716765963"
@@ -13,7 +13,7 @@ const Page: React.FC = () => {
       }}
     />
 
-  <Elections />
+    <Elections />
   </div>
 };
 

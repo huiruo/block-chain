50aac6811ff71ab6d4d0973759e14136030a5d39
ruo <ruomechen@gmail.com>
2024-11-05 14:12:54 +0800

    feat: add election

---
+++ src/app/elections/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Page: React.FC = () => {
+  return <div style={{marginTop: '60px'}}>
+    <iframe
+      title="election-map"
+      src="https://polymarket.com/embed/elections/map?event=presidency&theme=dark&creator=0x841e9627B67Ca2E957e8c55B4cF28Ec43B93651D-1730716765963"
+      width="100%"
+      height="600px"
+      style={{
+        height: '60vh'
+      }}
+    />
+  </div>
+};
+
+export default Page

de5148b9cb0f0515d0b56e93e01483fc93c11f36
ruo <ruomechen@gmail.com>
2025-09-15 16:45:34 +0800

    feat: realTrade

---
+++ src/app/trade/realTrade/page.tsx
@@ -0,0 +1,7 @@
+import RealTrade from '../../../mdx/trade/realTrade.mdx';
+
+const Page: React.FC = () => {
+  return <RealTrade />
+};
+
+export default Page
--- src/common/router.ts
+++ src/common/router.ts
@@ -15,6 +15,7 @@ export const sidebar: ISidebar[] = [
     href: '/trade',
     type: 'category',
     items: [
+      { href: "/trade/realTrade", label: "Real Trade" },
       { href: "/trade/trade", label: "trade" },
       { href: "/trade/trade2", label: "trade2" }
     ]
